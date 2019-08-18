45069db743c9622b98d69566b3def4b568ab3956
min <msms9957@gmail.com>
2019-08-18 15:46:36 +0900

    0818

--- flask/mypage_test/flaskapp/__init__.py
+++ flask/mypage_test/flaskapp/__init__.py
@@ -29,7 +29,7 @@ def tutor_lecture():
     cursor.execute(query)
     data3=(cursor.fetchall())
 
-    #튜터>출결현황그래프
+    #튜터>출결현황그래프(각 인원수)
     query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING,CLASS_INFO WHERE STATUS = 'pass~~' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND TUTEE_CLASS_MAPPING.CLASS_ID=CLASS_INFO.CLASS_ID; "
     cursor.execute(query)
     data4=(cursor.fetchall())
@@ -41,16 +41,34 @@ def tutor_lecture():
     query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING,CLASS_INFO WHERE STATUS = 'FAIL' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND TUTEE_CLASS_MAPPING.CLASS_ID=CLASS_INFO.CLASS_ID;"
     cursor.execute(query)
     data6=(cursor.fetchall())
-    #c튜터>출결현황그래프>총인원
-    query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING,CLASS_INFO WHERE TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND TUTEE_CLASS_MAPPING.CLASS_ID=CLASS_INFO.CLASS_ID AND STATUS = 'pass~~' OR STATUS='LATE' OR STATUS='FAIL'; "
+
+    #튜터>출결현황그래프>총인원
+    query = "SELECT COUNT(CLASS_ID) FROM TUTEE_CLASS_MAPPING WHERE TUTEE_CLASS_MAPPING.CLASS_ID='1';"
     cursor.execute(query)
     data7=(cursor.fetchall())
 
-    #튜터>출결현황그래프>pass %
-    query = "SELECT COUNT(STATUS)/data7 FROM ATTENDANCE,TUTEE_CLASS_MAPPING,CLASS_INFO WHERE TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND TUTEE_CLASS_MAPPING.CLASS_ID=CLASS_INFO.CLASS_ID AND STATUS = 'pass~~'; "
+    #튜터>출결현황그래프>날짜(최신,달력)
+    query = "SELECT max(DATE) FROM ATTENDANCE WHERE DATE IN (SELECT max(DATE) FROM ATTENDANCE)"
     cursor.execute(query)
     data8=(cursor.fetchall())
 
+    #출석, 지각, 결석 퍼센트 쿼리문
+    query = "SELECT COUNT(STATUS) FROM ATTENDANCE WHERE TUTEE_CLASS_MAPPING.CLASS_ID='1' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND ATTENDANCE.DATE=%s AND ATTENDANCE.STATUS=%s;"
+    value=(data8,'pass~~')
+    cursor.execute(query,value)
+    data9=(cursor.fetchall())
+    
+    query = "SELECT COUNT(STATUS) FROM ATTENDANCE WHERE TUTEE_CLASS_MAPPING.CLASS_ID='1' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND ATTENDANCE.DATE=%s AND ATTENDANCE.STATUS=%s;"
+    value=(data8,'LATE')
+    cursor.execute(query,value)
+    data10=(cursor.fetchall())
+
+    query = "SELECT COUNT(STATUS) FROM ATTENDANCE WHERE TUTEE_CLASS_MAPPING.CLASS_ID='1' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND ATTENDANCE.DATE=%s AND ATTENDANCE.STATUS=%s;"
+    value=(data8,'FAIL')
+    cursor.execute(query,value)
+    data11=(cursor.fetchall())
+
+    cursor.execute
     datalist=[]       
     for row in data:
         if row :        #튜터마이페이지 > 강의목록
@@ -87,11 +105,20 @@ def tutor_lecture():
             dic={'COUNT(STATUS)':row[0]}
             datalist.append(dic)
 
-    for row in data8:  
+    for row in data9:  
         if row :        #튜터마이페이지 > 출결현황(출석퍼센트)
-            dic={'COUNT(STATUS)/data7':row[0]}
+            dic={'pass%':row[0]}
             datalist.append(dic)
 
+    for row in data10:  
+        if row :        #튜터마이페이지 > 출결현황(지각퍼센트)
+            dic={'late%':row[0]}
+            datalist.append(dic)
+    for row in data11:  
+        if row :        #튜터마이페이지 > 출결현황(결석퍼센트)
+            dic={'fail%':row[0]}
+            datalist.append(dic)
+            
     cursor.close()
     db.close()
     return json.dumps(datalist)
