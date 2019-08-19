5b536b7b6a0c51fbd0333758c0dfdadec7a734ec
min <msms9957@gmail.com>
2019-08-19 19:34:59 +0900

    080808080

--- flask/mypage_test/flaskapp/__init__.py
+++ flask/mypage_test/flaskapp/__init__.py
@@ -4,7 +4,7 @@ import pymysql,json, hashlib
 
 app=Flask(__name__)
 app.debug=True
-
+app.secret_key("temporary string")
 @app.before_request
 def befor_request():
     session.permanent=True
@@ -122,135 +122,164 @@ def tutorStudent():
 @app.route("/tutorCalendar",methods=["POST","GET"])
 #_튜터>달력
 def tutorCalendar():
-    db = pymysql.connect(host='127.0.0.1',
-        port=3306,
-        user='admin',
-        passwd='0507',
-        db='attendance',
-        charset='utf8')
-    cursor=db.cursor()
+    if 'username' in session:
+        result = '%s'%escape (session['username'])
+        db = pymysql.connect(host='127.0.0.1',
+          port=3306,
+          user='admin',
+          passwd='0507',
+          db='attendance',
+          charset='utf8')
+        cursor=db.cursor()
+        query = "SELECT TUTOR_ID FROM TUTOR_INFO WHERE EMAIL = %s"
+        value = (result)
+        cursor.execute(query,value)
+        key = (cursor.fetchall())
+        
+        for row in key :
+            key = row[0]
 
-    #튜터>달력
-    query="SELECT CLASS_INFO.CLASS_NAME,CLASS_INFO.CLASS_TIME,CLASS_INFO.CLASS_ROOM,ATTENDANCE.DATE FROM CLASS_INFO,ATTENDANCE,TUTOR_INFO,TUTEE_CLASS_MAPPING WHERE ATTENDANCE.MAPPING_ID = TUTEE_CLASS_MAPPING.MAPPING_ID AND CLASS_INFO.CLASS_ID=TUTEE_CLASS_MAPPING.CLASS_ID AND CLASS_INFO.TUTOR_ID=TUTOR_INFO.TUTOR_ID"
-    cursor.execute(query)
-    data3=(cursor.fetchall())
+        if key:
+           #튜터>달력
+            query="SELECT CLASS_INFO.CLASS_ID,CLASS_NAME,CLASS_TIME,DATE FROM ATTENDANCE,CLASS_INFO,TUTEE_CLASS_MAPPING WHERE TUTOR_ID=%s AND CLASS_INFO.CLASS_ID=TUTEE_CLASS_MAPPING.CLASS_ID AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID"
+            value=(key)
+            cursor.execute(query,value)
+            data3=(cursor.fetchall())
 
-    datalist=[]
-    for row in data3:
-        if row :        #튜터마이페이지 > 캘린더
-            dic={'CLASS_INFO.CLASS_NAME':row[0:],'CLASS_INFO.CLASS_TIME':row[0:],'CLASS_INFO.CLASS_ROOM':row[0:],'ATTENDANCE.DATE':row[0:]}
-            datalist.append(dic)
-    DATA={'CLASS_NAME,TIME,ROOM,DATE':'%s'%datalist}
-    i = json.dumps(DATA)
-    loaded_i = json.loads(i)
-    cursor.close()
-    db.close()
-    return loaded_i
+            datalist=[]
+            
+            for row in data3:
+                if row :        #튜터마이페이지 > 캘린더
+                    dic={'CLASS_ID/NAME/TIME/DATE':row[0:]}
+                    datalist.append(dic)
+            DATA={'CLASS_ID,NAME,TIME,DATE':'%s'%datalist}
+            i = json.dumps(DATA)
+            loaded_i = json.loads(i)
+            cursor.close()
+            db.close()
+            return loaded_i
+    else: #튜터 아닐시
+        error = {'error':'error!error!error!'}
+        r = json.dumps(error)
+        loaded_r = json.loads(r)
+        return loaded_r
 
 @app.route("/tutorOgraph",methods=["POST","GET"])
 #_튜터>출결현황그래프(각 인원수)
 def tutorOgraph():
-    db = pymysql.connect(host='127.0.0.1',
-        port=3306,
-        user='admin',
-        passwd='0507',
-        db='attendance',
-        charset='utf8')
-    cursor=db.cursor()
+    if 'username' in session:
+        result = '%s'%escape (session['username'])
+        db = pymysql.connect(host='127.0.0.1',
+          port=3306,
+          user='admin',
+          passwd='0507',
+          db='attendance',
+          charset='utf8')
+        cursor=db.cursor()
+        query = "SELECT TUTOR_ID FROM TUTOR_INFO WHERE EMAIL = %s"
+        value = (result)
+        cursor.execute(query,value)
+        key = (cursor.fetchall())
+        
+        for row in key :
+            key = row[0]
+
+        if key:
     
-    #튜터>출결현황그래프(각 인원수)
-    query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING,CLASS_INFO WHERE STATUS = 'pass~~' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND TUTEE_CLASS_MAPPING.CLASS_ID=CLASS_INFO.CLASS_ID; "
-    cursor.execute(query)
-    data4=(cursor.fetchall())
-    datalist=[]
-    for row in data4:   
-        if row :        #튜터마이페이지 > 출결현황(출석)
-            dic={'COUNT(STATUS)':row[0]}
-            datalist.append(dic)
-    for row in data4:
-      data4=row[0]
+            #튜터>출결현황그래프(각 인원수)
+            query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING,CLASS_INFO WHERE STATUS = 'pass~~' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND TUTEE_CLASS_MAPPING.CLASS_ID=CLASS_INFO.CLASS_ID; "
+            cursor.execute(query)
+            data4=(cursor.fetchall())
+            datalist=[]
+            for row in data4:   
+                if row :        #튜터마이페이지 > 출결현황(출석)
+                    dic={'COUNT(STATUS)':row[0]}
+                    datalist.append(dic)
+            DATA={'출석':'%s'%datalist}
+            for row in data4:
+              data4=row[0]
 
-    query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING,CLASS_INFO WHERE STATUS = 'LATE' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND TUTEE_CLASS_MAPPING.CLASS_ID=CLASS_INFO.CLASS_ID;"
-    cursor.execute(query)
-    data5=(cursor.fetchall())
-    for row in data5:  
-        if row :        #튜터마이페이지 > 출결현황(지각)
-            dic={'COUNT(STATUS)':row[0]}
-            datalist.append(dic)
-    for row in data5:
-      data5=row[0]
+            query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING,CLASS_INFO WHERE STATUS = 'LATE' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND TUTEE_CLASS_MAPPING.CLASS_ID=CLASS_INFO.CLASS_ID;"
+            cursor.execute(query)
+            data5=(cursor.fetchall())
+            for row in data5:  
+                if row :        #튜터마이페이지 > 출결현황(지각)
+                    dic={'COUNT(STATUS)':row[0]}
+                    datalist.append(dic)
+            for row in data5:
+              data5=row[0]
 
-    query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING,CLASS_INFO WHERE STATUS = 'FAIL' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND TUTEE_CLASS_MAPPING.CLASS_ID=CLASS_INFO.CLASS_ID;"
-    cursor.execute(query)
-    data6=(cursor.fetchall())
-    for row in data6:  
-        if row :        #튜터마이페이지 > 출결현황(결석)
-            dic={'COUNT(STATUS)':row[0]}
-            datalist.append(dic)
-    for row in data6:
-      data6=row[0]
+            query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING,CLASS_INFO WHERE STATUS = 'FAIL' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND TUTEE_CLASS_MAPPING.CLASS_ID=CLASS_INFO.CLASS_ID;"
+            cursor.execute(query)
+            data6=(cursor.fetchall())
+            for row in data6:  
+                if row :        #튜터마이페이지 > 출결현황(결석)
+                    dic={'COUNT(STATUS)':row[0]}
+                    datalist.append(dic)
+            for row in data6:
+              data6=row[0]
 
-    #튜터>출결현황그래프>총인원
-    query = "SELECT COUNT(CLASS_ID) FROM TUTEE_CLASS_MAPPING WHERE TUTEE_CLASS_MAPPING.CLASS_ID='1';"
-    cursor.execute(query)
-    data7=(cursor.fetchall())
-    for row in data7:  
-        if row :        #튜터마이페이지 > 출결현황(총인원)
-            dic={'COUNT(STATUS)':row[0]}
-            datalist.append(dic)
-    for row in data7:
-      data7=row[0]
-    
-    #튜터>출결현황그래프>날짜(최신,달력)
-    query = "SELECT max(DATE) FROM ATTENDANCE WHERE DATE IN (SELECT max(DATE) FROM ATTENDANCE)"
-    cursor.execute(query)
-    data8=(cursor.fetchall())
+            #튜터>출결현황그래프>총인원
+            query = "SELECT COUNT(CLASS_ID) FROM TUTEE_CLASS_MAPPING WHERE TUTEE_CLASS_MAPPING.CLASS_ID='1';"
+            cursor.execute(query)
+            data7=(cursor.fetchall())
+            for row in data7:  
+                if row :        #튜터마이페이지 > 출결현황(총인원)
+                    dic={'COUNT(STATUS)':row[0]}
+                    datalist.append(dic)
+            for row in data7:
+              data7=row[0]
+            
+            #튜터>출결현황그래프>날짜(최신,달력)
+            query = "SELECT max(DATE) FROM ATTENDANCE WHERE DATE IN (SELECT max(DATE) FROM ATTENDANCE)"
+            cursor.execute(query)
+            data8=(cursor.fetchall())
 
-    #출석, 지각, 결석 퍼센트 쿼리문
-    query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING WHERE TUTEE_CLASS_MAPPING.CLASS_ID='1' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND ATTENDANCE.DATE=%s AND ATTENDANCE.STATUS='pass~~';"
-    value=(data8)
-    cursor.execute(query,value)
-    data9=(cursor.fetchall())
-    percent=(cursor.fetchall())
-    for row in percent:   #출석 퍼센트
-      percent=row[0]
-    percent=(int)((data4/data7)*100)
-    for row in data9:  
-        if row :        #튜터마이페이지 > 출결현황(출석퍼센트)
-            dic={'pass%':'%s'%percent}
-            datalist.append(dic)
+            #출석, 지각, 결석 퍼센트 쿼리문
+            query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING WHERE TUTEE_CLASS_MAPPING.CLASS_ID='1' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND ATTENDANCE.DATE=%s AND ATTENDANCE.STATUS='pass~~';"
+            value=(data8)
+            cursor.execute(query,value)
+            data9=(cursor.fetchall())
+            percent=(cursor.fetchall())
+            for row in percent:   #출석 퍼센트
+              percent=row[0]
+            percent=(int)((data4/data7)*100)
+            for row in data9:  
+                if row :        #튜터마이페이지 > 출결현황(출석퍼센트)
+                    dic={'pass%':'%s'%percent}
+                    datalist.append(dic)
 
-    query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING WHERE TUTEE_CLASS_MAPPING.CLASS_ID='1' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND ATTENDANCE.DATE=%s AND ATTENDANCE.STATUS='LATE';"
-    value=(data8)
-    cursor.execute(query,value)
-    data10=(cursor.fetchall())
-    percent2=(cursor.fetchall())
-    for row in percent2:  #지각 퍼센트
-      percent2=row[0]
-    percent2=(int)((data5/data7)*100)
-    for row in data10:  
-        if row :        #튜터마이페이지 > 출결현황(지각퍼센트)
-            dic={'late%':'%s'%percent2}
-            datalist.append(dic)
+            query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING WHERE TUTEE_CLASS_MAPPING.CLASS_ID='1' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND ATTENDANCE.DATE=%s AND ATTENDANCE.STATUS='LATE';"
+            value=(data8)
+            cursor.execute(query,value)
+            data10=(cursor.fetchall())
+            percent2=(cursor.fetchall())
+            for row in percent2:  #지각 퍼센트
+              percent2=row[0]
+            percent2=(int)((data5/data7)*100)
+            for row in data10:  
+                if row :        #튜터마이페이지 > 출결현황(지각퍼센트)
+                    dic={'late%':'%s'%percent2}
+                    datalist.append(dic)
 
-    query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING WHERE TUTEE_CLASS_MAPPING.CLASS_ID='1' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND ATTENDANCE.DATE=%s AND ATTENDANCE.STATUS='FAIL';"
-    value=(data8)
-    cursor.execute(query,value)
-    data11=(cursor.fetchall())
-    percent3=(cursor.fetchall())
-    for row in percent3:  #결석 퍼센트
-      percent3=row[0]
-    percent3=(int)((data6/data7)*100)
-    for row in data11:  
-        if row :        #튜터마이페이지 > 출결현황(결석퍼센트)
-            dic={'fail%':'%s'%percent3}
-            datalist.append(dic)
+            query = "SELECT COUNT(STATUS) FROM ATTENDANCE,TUTEE_CLASS_MAPPING WHERE TUTEE_CLASS_MAPPING.CLASS_ID='1' AND TUTEE_CLASS_MAPPING.MAPPING_ID=ATTENDANCE.MAPPING_ID AND ATTENDANCE.DATE=%s AND ATTENDANCE.STATUS='FAIL';"
+            value=(data8)
+            cursor.execute(query,value)
+            data11=(cursor.fetchall())
+            percent3=(cursor.fetchall())
+            for row in percent3:  #결석 퍼센트
+              percent3=row[0]
+            percent3=(int)((data6/data7)*100)
+            for row in data11:
+                if row :        #튜터마이페이지 > 출결현황(결석퍼센트)
+                    dic={'fail%':'%s'%percent3}
+                    datalist.append(dic)
 
-    i = json.dumps(dic)
-    loaded_i = json.loads(i)
-    cursor.close()
-    db.close()
-    return loaded_i
+            i = json.dumps(dic)
+            loaded_i = json.loads(i)
+            cursor.close()
+            db.close()
+            return loaded_i
 
 @app.route("/tutorLgraph",methods=["POST","GET"])
 #_튜터>평균그래프(각 인원수)
@@ -380,24 +409,50 @@ def sessionCheck():
 
 @app.route("/tuteeMypage",methods=["POST","GET"])
 def tutee_lecture():
-    db = pymysql.connect(host='127.0.0.1',
-        port=3306,
-        user='admin',
-        passwd='0507',
-        db='attendance',
-        charset='utf8')
-    cursor=db.cursor()
-    #튜티>강의목록
-    query = "SELECT CLASS_INFO.CLASS_NAME FROM CLASS_INFO,TUTEE_INFO,TUTEE_CLASS_MAPPING WHERE TUTEE_INFO.TUTEE_ID=TUTEE_CLASS_MAPPING.TUTEE_ID AND TUTEE_CLASS_MAPPING.CLASS_ID=CLASS_INFO.CLASS_ID;"
-    cursor.execute(query)
-    data=(cursor.fetchall())
+    if 'username' in session:
+        result = '%s'%escape (session['username'])
+        db = pymysql.connect(host='127.0.0.1',
+          port=3306,
+          user='admin',
+          passwd='0507',
+          db='attendance',
+          charset='utf8')
+        cursor=db.cursor()
+        query = "SELECT TUTEE_ID FROM TUTOR_INFO WHERE EMAIL = %s" 
+        value = (result)
+        cursor.execute(query,value)
+        key = (cursor.fetchall())
+        
+        for row in key :
+            key = row[0]
 
-    datalist=[]       
-    for row in data:
-        if row :  #튜티마이페이지 > 강의목록
-            dic={'CLASS_INFO.CLASS_NAME':row[0:]}
-            datalist.append(dic)
+        if key:
+            #튜티>강의목록
+            query = "SELECT CLASS_INFO.CLASS_NAME FROM CLASS_INFO,TUTEE_INFO,TUTEE_CLASS_MAPPING WHERE TUTEE_INFO.TUTEE_ID=TUTEE_CLASS_MAPPING.TUTEE_ID AND TUTEE_CLASS_MAPPING.CLASS_ID=CLASS_INFO.CLASS_ID;"
+            cursor.execute(query)
+            data=(cursor.fetchall())
 
-    cursor.close()
-    db.close()
-    return json.dumps(datalist)
+            datalist=[]       
+            for row in data:
+                if row :  #튜티마이페이지 > 강의목록
+                    dic={'CLASS_NAME':row[0:]}
+                    datalist.append(dic)
+            DATA={'CLASS_NAME':'%s'%datalist}
+            i = json.dumps(DATA)
+            loaded_i = json.loads(DATA)
+            cursor.close()
+            db.close()
+            return loaded_i
+        else : 
+            error = {'error':'error!error!error!'}
+            r = json.dumps(error)
+            loaded_r = json.loads(r)
+            cursor.close()
+            db.close()
+            return loaded_r
+
+    else: #로그인 안됐을때 접근제한 처리
+        error = {'error':'error!error!error!'}
+        r = json.dumps(error)
+        loaded_r = json.loads(r)
+        return loaded_r
